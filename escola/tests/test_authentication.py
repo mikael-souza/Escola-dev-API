8c6953454d35a2b3fac1983e51e054100a8614aa
mikael-souza <fmikael931@gmail.com>
2024-11-20 13:41:01 -0300

    Testes de autenticação na requisição GET

--- escola/tests/test_authentication.py
+++ escola/tests/test_authentication.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from django.contrib.auth import authenticate
+from django.urls import reverse
+from rest_framework import status
 
 class AuthenticationUserTestCase(APITestCase):
     def setUp(self):
         self.usuario = User.objects.create_superuser(username='admin',password='admin')
+        self.url = reverse('Estudantes-list')
     
     def test_autenticacao_user_com_credenciais_corretas(self):
         """Teste que verifica a autenticação de um user com as credenciais corretas"""
@@ -20,4 +23,10 @@ class AuthenticationUserTestCase(APITestCase):
         """Teste que verifica a autenticação com usenha incorreto"""
         usuario = authenticate(username = 'admn',password='adm')
         self.assertFalse((usuario is not None) and usuario.is_authenticated)
+
+    def teste_requisicao_get_autorizada(self):
+        """Teste que verifica uma requisição GET autorizada"""
+        self.client.force_authenticate(self.usuario)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
